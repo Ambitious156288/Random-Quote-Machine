554995119b1161c20f877712f0955d7bd94687ae
Krystian Kalwik <ambitious156288@gmail.com>
2020-05-12 18:55:28 +0200

    add data

---
+++ src/array/Array.js
@@ -0,0 +1,13 @@
+export const array1 = [
+  '"Every child is an artist. The problem is how to remain an artist once he grows up."',
+  '"Fall seven times and stand up eight."',
+  '"Start where you are. Use what you have. Do what you can."',
+  '"Education costs money. But then so does ignorance."',
+];
+
+export const array2 = [
+  '- Pablo Picasso',
+  '- Japanese Proverb',
+  '- Arthur Ashe',
+  '- Sir Claus Moser',
+];
---
+++ src/theme/theme.js
@@ -0,0 +1,19 @@
+export const theme0 = {
+  default: '#16a085',
+};
+
+export const theme1 = {
+  default: '#2c3e50',
+};
+
+export const theme2 = {
+  default: '#27ae60 ',
+};
+
+export const theme3 = {
+  default: '#f39c12',
+};
+
+export const theme4 = {
+  default: '#e74c3c',
+};
