d037f5ce6025147a11aa71b5267e0f0c07ca1c3e
Krystian Kalwik <ambitious156288@gmail.com>
2020-05-12 23:57:42 +0200

    trying to fix globalStyles deployment problem

--- src/theme/Background.js
+++ src/theme/Background.js
@@ -1,8 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 const Background = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap');
-    
+        /* @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap'); */
+
     body{ 
         background-color: ${({ theme }) => theme.default};
         transition: 0.7s;
