7edefaffa6c1672a282519c3e3f511d1aedc59e0
Krystian Kalwik <ambitious156288@gmail.com>
2020-05-12 23:42:54 +0200

    Updates

---
+++ precache-manifest.ea6141213a19908e89139cd1c7c2914a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "721113564d4ce3ab70d2",
+    "url": "/Random-Quote-Machine/static/js/main.42c3f652.chunk.js"
+  },
+  {
+    "revision": "fc1abb5e9f8eb11e4c02",
+    "url": "/Random-Quote-Machine/static/js/runtime~main.6010cb3b.js"
+  },
+  {
+    "revision": "9d79edf9f2d21278f815",
+    "url": "/Random-Quote-Machine/static/js/2.9ee4b7ed.chunk.js"
+  },
+  {
+    "revision": "ab398af369e6ef345676e8b4d019193b",
+    "url": "/Random-Quote-Machine/index.html"
+  }
+];
